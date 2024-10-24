96d3cf73d7c6b011b272b3a424ffebf3801e49de
Andychen <rd-sw18@visionsecurity.com.tw>
2024-10-24 15:55:07 +0800

    feat: 測試bundle找不到的異常

--- iHubSDK/iHubLanguageManager.swift
+++ iHubSDK/iHubLanguageManager.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+
 public class iHubLanguageManager {
     public static let shared = iHubLanguageManager()
     public var onUpdateLanguageFlow: (() -> Void)?
@@ -44,5 +45,33 @@ public class iHubLanguageManager {
             self.onUpdateLanguageFlow?()
         }
     }
+}
+
+extension Bundle {
+    static var mySDK: Bundle {
+        return Bundle(for: iHubLanguageManager.self)
+    }
+    private static var bundles: [String: Bundle] = [:]
     
+    public class func setLanguage(_ language: String) {
+        defer {
+            object_setClass(Bundle.main, AnyLanguageBundle.self)
+        }
+        
+        guard let path = Bundle.main.path(forResource: language, ofType: "lproj"),
+              let bundle = Bundle(path: path) else {
+            return
+        }
+        
+        bundles[language] = bundle
+    }
+    
+    class AnyLanguageBundle: Bundle {
+        override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+            if let bundle = Bundle.bundles[iHubLanguageManager.shared.currentLanguage] {
+                return bundle.localizedString(forKey: key, value: value, table: tableName)
+            }
+            return super.localizedString(forKey: key, value: value, table: tableName)
+        }
+    }
 }
